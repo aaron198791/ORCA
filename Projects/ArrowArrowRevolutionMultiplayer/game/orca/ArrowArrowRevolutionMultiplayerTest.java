d0361830cd0566c5eb5a88e0988107367922881c
aaron198791 <42753836+aaron198791@users.noreply.github.com>
2018-11-18 00:32:10 -0500

    update

--- Projects/ArrowArrowRevolutionMultiplayer/game/orca/ArrowArrowRevolutionMultiplayerTest.java
+++ Projects/ArrowArrowRevolutionMultiplayer/game/orca/ArrowArrowRevolutionMultiplayerTest.java
@@ -1,8 +1,22 @@
 package game.orca;
+import javax.swing.JFileChooser;
+import java.awt.Container;
+import  java.awt.Component;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 public class ArrowArrowRevolutionMultiplayerTest{
 public static void main(String[] args)
 {
-ArrowArrowRevolutionMultiplayer run = new ArrowArrowRevolutionMultiplayer();
-run.game();
+	JFrame frame = new JFrame();
+JFileChooser chooser = new JFileChooser();
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images", "jpg", "gif");
+    chooser.setFileFilter(filter);
+    int returnVal = chooser.showOpenDialog(frame);
+    if(returnVal == JFileChooser.APPROVE_OPTION) {
+       System.out.println("You chose to open this file: " +
+            chooser.getSelectedFile().getName());
+    }
+/*ArrowArrowRevolutionMultiplayer run = new ArrowArrowRevolutionMultiplayer();
+run.game();*/
 }
 }
