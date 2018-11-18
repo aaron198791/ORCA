60451eab3fe398d82ccf9c0b1c33184e1a58136c
aaron198791 <42753836+aaron198791@users.noreply.github.com>
2018-11-17 22:33:09 -0500

    update

--- Projects/ArrowArrowRevolutionMultiplayer/game/orca/ArrowArrowRevolutionMultiplayer.java
+++ Projects/ArrowArrowRevolutionMultiplayer/game/orca/ArrowArrowRevolutionMultiplayer.java
@@ -6,7 +6,22 @@ import java.io.*;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import java.awt.Toolkit;
 
 public class ArrowArrowRevolutionMultiplayer {
-	// insert code here
-}
+	public void game(){
+	JFrame jframe = new JFrame("ORCA Game");// insert code here
+	 jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    // set the jframe size and location, and make it visible
+   Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    jframe.setSize(screenSize	.width, screenSize.height);
+    jframe.pack();
+    jframe.setLocationRelativeTo(null);
+    jframe.setVisible(true);
+	jframe.setVisible(true);
+	}
+	}
