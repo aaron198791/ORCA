5156fe5c08d762a52b24ee652d4b129aa79e29e7
angel8818 <42220654+angel8818@users.noreply.github.com>
2018-11-26 17:38:35 -0500

    test for class

---
+++ Projects/ArrayPlayground/sketch/orca/TwoDArrayInitialization.java
@@ -0,0 +1,15 @@
+package sketch.orca;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class TwoDArrayInitialization {
+	public static void main(String[] args) {
+		int[][] twoD = new int[3];
+		
+		twoD[0] = new int[3];
+		twoD[1] = new int[4];
+		twoD[2] = new int[2];
+	}
+}
