e213d1ae206058df5e164723be96393e8297c7ec
angel8818 <42220654+angel8818@users.noreply.github.com>
2018-12-05 15:58:54 -0500

    I tried writing a sketch to test something; compilation failure

---
+++ Projects/MessingWithConstructors/sketch/orca/StringvScannerConstructor.java
@@ -0,0 +1,12 @@
+package sketch.orca;
+
+import java.lang.*;
+import java.util.*;
+
+public class StringvScannerConstructor {
+	public static void main(String[] args) {
+		String str = new String(System.in);
+		
+		System.out.println(str);
+	}
+}
