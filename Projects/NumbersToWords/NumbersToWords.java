c5ed1cfe055ee5cdf1f97e2d4d0f33947192c2a4
AmramT <atorczyn@mail.yu.edu>
2018-12-09 20:45:08 -0500

    first commit

---
+++ Projects/NumbersToWords/NumbersToWords.java
@@ -0,0 +1,17 @@
+package orca.fred.projects;
+import java.util.Scanner;
+
+public class NumbersToWords{
+	public static String translate(int x){
+		String str = "";
+		while(x >= 10){
+		}
+		while(x < 10){
+		}
+		return str;
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+	}
+}
