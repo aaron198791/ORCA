312e922726ef2e8925dc5bd52d3150774b8fe491
angel8818 <42220654+angel8818@users.noreply.github.com>
2018-12-09 20:31:08 -0500

    suicide

---
+++ Projects/SystemIn/sketch/orca/SystemIn.java
@@ -0,0 +1,11 @@
+package sketch.orca;
+
+import java.util.*;
+import java.lang.*;
+
+public class SystemIn {
+	public static void main(String[] args) {
+		String str = new Scanner(toString(System.in));
+		System.out.println(str);
+	}
+}
