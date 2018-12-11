ee1b0349072b994e73f87d5ad8ed6bf7c311bbe7
aaron198791 <42753836+aaron198791@users.noreply.github.com>
2018-12-10 23:24:37 -0500

    farts

--- Projects/UUID_team/project/member/UUID_names.java
+++ Projects/UUID_team/project/member/UUID_names.java
@@ -2,6 +2,9 @@ package project.member;
 import java.util.*;
 public class UUID_names
 {
+	/*
+	please ensure to record and keep your UUID safe
+	*/
 	public UUID_names(){}
 	public UUID create_ID()
 	{
