e21e1e5d83955e0b1677240fd54b6d01f17faac5
aaron198791 <42753836+aaron198791@users.noreply.github.com>
2018-12-10 23:49:19 -0500

    don't not touch

--- Projects/UUID_team/API/orca/UUID_names.java
+++ Projects/UUID_team/API/orca/UUID_names.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
+import java.io.*;
 public class UUID_names {
 	
 	/*
@@ -21,10 +22,16 @@ public class UUID_names {
 	public UUID create_ID() {
 		UUID uuid =UUID.randomUUID();
 		return uuid;
-String usernameComputer3=System.getProperty("user.dir");
+try(
+		String usernameComputer3=System.getProperty("user.dir");
 PrintWriter writer = new PrintWriter(usernameComputer3+"/Do_Not_open.txt", "UTF-8");
 			writer.println(uuid.toString());
 			writer.close();		
+	}
+	catch (FileNotFoundException e)
+	{
+		System.out.println("couldn't find path");
+	}
 	}
 	
 	public static void main(String[] args) {
