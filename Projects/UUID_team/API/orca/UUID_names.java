611e203e6e526778e33baad5eb21feb388c3af09
AmramT <atorczyn@mail.yu.edu>
2018-12-12 13:41:07 -0500

    revisions

--- Projects/UUID_team/API/orca/UUID_names.java
+++ Projects/UUID_team/API/orca/UUID_names.java
@@ -14,10 +14,11 @@ public class UUID_names {
 	
 	/*
 	please ensure to record and keep your UUID safe
-	please only instanticate once!
-	I will keep track of who instanciates 
+	please only instantiate once!
+	I will keep track of who instantiates 
 	*/
-	public UUID_names() {}
+	public UUID_names() {
+	}
 	
 	public UUID create_ID() {
 		UUID uuid =UUID.randomUUID();
@@ -31,7 +32,6 @@ PrintWriter writer = new PrintWriter(usernameComputer3+"/Do_Not_open.txt", "UTF-
 	catch (FileNotFoundException e)
 	{
 		System.out.println("couldn't find path");
-	}
 	}
 	
 	public static void main(String[] args) {
