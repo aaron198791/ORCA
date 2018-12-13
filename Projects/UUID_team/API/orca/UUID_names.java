d3424b8449e349edc4a3ee96e30a30c62fc4aa04
aaron198791 <42753836+aaron198791@users.noreply.github.com>
2018-12-13 01:17:37 -0500

    don't not touch

--- Projects/UUID_team/API/orca/UUID_names.java
+++ Projects/UUID_team/API/orca/UUID_names.java
@@ -6,6 +6,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.*;
 public class UUID_names {
+	public static void main(String[] args) {
+		System.out.println("Successfully compiled UUID_names API");
+		UUID_names name = new UUID_names();
+		name.create_ID();
+	}
 	
 	/*
 	please ensure to record and keep your UUID safe
@@ -15,34 +20,22 @@ public class UUID_names {
 	public UUID_names() {
 	}
 	
-	public UUID create_ID() {
+	public void create_ID() {
 		UUID uuid = UUID.randomUUID();
-		return uuid;
-<<<<<<< HEAD
-<<<<<<< HEAD
-try(
+		//return uuid;
+
+try{
 		String usernameComputer3=System.getProperty("user.dir");
 		String usernameComputer4=System.getProperty("user");
 PrintWriter writer = new PrintWriter(usernameComputer3+"/Do_Not_open.txt", "UTF-8");
-			String cool = uuid.toString();
-			writer.println(cool);
-			writer.println(usernameComputer4);
-=======
-=======
->>>>>>> a5c74857456fd6386e10e6a18cb5960317a0343f
-		try{//Unreachable statement
-			String usernameComputer3 = System.getProperty("user.dir");
-			PrintWriter writer = new PrintWriter(usernameComputer3 + "/Do_Not_open.txt", "UTF-8");
 			writer.println(uuid.toString());
->>>>>>> a5c74857456fd6386e10e6a18cb5960317a0343f
+			writer.println(usernameComputer4);
 			writer.close();		
-		}
+}
 		catch (FileNotFoundException | UnsupportedEncodingException e){//Note the second
 			System.out.println("couldn't find path");
 		}
 	}//Supposedly missing return statement; interesting because there IS one earlier, and it's causing its own problems
-	public static void main(String[] args) {
-		System.out.println("Successfully compiled UUID_names API");
-	}
+	
 	
 }
