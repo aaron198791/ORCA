b63a5f8caf37a536686d5af9fb89bdc4daa3635c
aaron198791 <42753836+aaron198791@users.noreply.github.com>
2018-12-13 00:15:27 -0500

    update

--- Projects/UUID_team/API/orca/UUID_names.java
+++ Projects/UUID_team/API/orca/UUID_names.java
@@ -1,27 +1,24 @@
 package API.orca;
 
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
 import java.io.*;
 public class UUID_names {
 	
 	/*
 	please ensure to record and keep your UUID safe
-	please only instanticate once!
-	I will keep track of who instanciates 
+	please only instantiate once!
+	I will keep track of who instantiates 
 	*/
-	public UUID_names() {}
+	public UUID_names() {
+	}
 	
 	public UUID create_ID() {
-		UUID uuid =UUID.randomUUID();
+		UUID uuid = UUID.randomUUID();
 		return uuid;
+<<<<<<< HEAD
 try(
 		String usernameComputer3=System.getProperty("user.dir");
 		String usernameComputer4=System.getProperty("user");
@@ -29,14 +26,18 @@ PrintWriter writer = new PrintWriter(usernameComputer3+"/Do_Not_open.txt", "UTF-
 			String cool = uuid.toString();
 			writer.println(cool);
 			writer.println(usernameComputer4);
+=======
+		try{//Unreachable statement
+			String usernameComputer3 = System.getProperty("user.dir");
+			PrintWriter writer = new PrintWriter(usernameComputer3 + "/Do_Not_open.txt", "UTF-8");
+			writer.println(uuid.toString());
+>>>>>>> a5c74857456fd6386e10e6a18cb5960317a0343f
 			writer.close();		
-	}
-	catch (FileNotFoundException e)
-	{
-		System.out.println("couldn't find path");
-	}
-	}
-	
+		}
+		catch (FileNotFoundException | UnsupportedEncodingException e){//Note the second
+			System.out.println("couldn't find path");
+		}
+	}//Supposedly missing return statement; interesting because there IS one earlier, and it's causing its own problems
 	public static void main(String[] args) {
 		System.out.println("Successfully compiled UUID_names API");
 	}
