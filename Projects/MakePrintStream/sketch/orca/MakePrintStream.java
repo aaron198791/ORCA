1a029e678f3cd33d620d25f2ef590190b79a4c78
angel8818 <42220654+angel8818@users.noreply.github.com>
2018-12-26 23:17:34 -0500

    im literally hackerman

---
+++ Projects/MakePrintStream/sketch/orca/MakePrintStream.java
@@ -0,0 +1,21 @@
+package sketch.orca;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class MakePrintStream {
+	private OutputStream os;	
+	private PrintStream ps = new PrintStream(os);
+	
+	private static void println(String str) {
+		System.out.println(str);
+	}
+	
+	public static void main(String[] args) {
+		println("fuqdasystem....literally");
+		println("mwahaha");
+		println("this is so dumb");
+		println("I will never need to write System.out ever again!!!");
+	}	
+}
