8fefb1e6d5e4eb6bdadbec219d9bb4063d9220c4
angel8818 <42220654+angel8818@users.noreply.github.com>
2019-01-03 20:21:55 -0500

    updated

--- Projects/ProjectedGradeOnCOM1300Final/sketch/orca/GetGrades.java
+++ Projects/ProjectedGradeOnCOM1300Final/sketch/orca/GetGrades.java
@@ -2,43 +2,79 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class GetGrades {
-	private static File[] files = new File("C:/Users/SP513/MYGIT/IntroToCompSci/assignments/").listFiles();
-	private static List <File> gradeFiles = new ArrayList <File> ();
-	
-	private GetGrades() {}
-
-	private static void findGradeFiles(File[] files) {
-		for(File f: files) {
-			if(f.isDirectory()) {
-				findGradeFiles(f.listFiles());
-			}
-			
-			else {
-				if(f.getName().startsWith("grade") && f.getName().endsWith(".txt")) {
-					gradeFiles.add(f);
+public class ProjectedGrade {
+	public static void main(String[] args) {
+		List <File> gradeFiles = GetGrades.getGradeFiles();
+		double[] hwScores = new double[20];	
+		double[] labScores = new double[5];
+		boolean needsChecking = true;
+		int j = 0;
+		int k = 0;
+		
+		try {				
+			for(File f: gradeFiles) {
+				Scanner sc = new Scanner(new FileReader(f));
+				needsChecking = true;
+				
+				while(sc.hasNext() && needsChecking) {
+					String newLine = sc.nextLine();
+					
+					if(newLine.contains("/10")) {
+						String[] splitLine = newLine.split("\\s");
+						
+						for(int i = 0; i < splitLine.length; i++) {
+							if(splitLine[i].contains("/10")) {
+								if(GetGrades.isLab.get(f)) {
+									labScores[k] = Double.valueOf(splitLine[i].substring(0, splitLine[i].indexOf("/")));
+									k++;
+								}
+								
+								if(! GetGrades.isLab.get(f)) {
+									hwScores[j] = Double.valueOf(splitLine[i].substring(0, splitLine[i].indexOf("/")));
+									j++;
+								}
+								
+								needsChecking = false;
+							}
+						}
+					}
 				}
 			}
+			
+			double avgHW = 0;
+			
+			for(double d: hwScores) {
+				avgHW += d;
+			}
+			
+			double avgLab = 0;
+			
+			for(double d: labScores) {
+				avgLab += d;
+			}
+			
+			avgHW /= 20;
+			avgHW *= .65;
+			avgHW *= 10;
+			
+			avgLab /= 5;
+			avgLab *= .1;
+			avgLab *= 10;
+			
+			System.out.print("ENTER YOUR DESIRED FINAL GRADE IN COM_1300: ");
+			Scanner scan = new Scanner(System.in);
+			double desiredGrade = scan.nextDouble();
+			
+			double requiredGrade = (desiredGrade - avgHW - avgLab) / .25;
+			
+			double maxGrade = 25 + avgHW + avgLab;
+			
+			System.out.printf("YOU WILL NEED A GRADE OF %.2f ON YOUR FINAL%n", requiredGrade);
+			System.out.printf("ASSUMING NO EXTRA-CREDIT ON THE FINAL, THE HIGHEST POSSIBLE GRADE IS %.2f%n", maxGrade);
 		}
-	}
-	
-	public static List <File> getGradeFiles() {
-		findGradeFiles(files);
 		
-		if(gradeFiles.size() == 25) {
-			return gradeFiles;
-		}
-		
-		else {
-			return null;
-		}
-	}
-	
-	public static void main(String[] args) {
-		getGradeFiles();
-		
-		for(File gF: gradeFiles) {
-			System.out.println(gF.getName());
+		catch(FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
 		}
 	}
 }
--- Projects/ProjectedGradeOnCOM1300Final/sketch/orca/ProjectedGrade.java
+++ Projects/ProjectedGradeOnCOM1300Final/sketch/orca/ProjectedGrade.java
@@ -5,9 +5,11 @@ import java.io.*;
 public class ProjectedGrade {
 	public static void main(String[] args) {
 		List <File> gradeFiles = GetGrades.getGradeFiles();
-		double[] score = new double[25];		
+		double[] hwScores = new double[20];	
+		double[] labScores = new double[5];
 		boolean needsChecking = true;
 		int j = 0;
+		int k = 0;
 		
 		try {				
 			for(File f: gradeFiles) {
@@ -22,38 +24,50 @@ public class ProjectedGrade {
 						
 						for(int i = 0; i < splitLine.length; i++) {
 							if(splitLine[i].contains("/10")) {
-								score[j] = Double.valueOf(splitLine[i].substring(0, splitLine[i].indexOf("/")));
+								if(GetGrades.isLab.get(f)) {
+									labScores[k] = Double.valueOf(splitLine[i].substring(0, splitLine[i].indexOf("/")));
+									k++;
+								}
+								
+								if(! GetGrades.isLab.get(f)) {
+									hwScores[j] = Double.valueOf(splitLine[i].substring(0, splitLine[i].indexOf("/")));
+									j++;
+								}
+								
 								needsChecking = false;
 							}
 						}
-						
-						j++;
 					}
 				}
 			}
 			
-			double avg = 0;
+			double avgHW = 0;
 			
-			for(double d: score) {
-				//System.out.println(d);
-				
-				avg += d;
+			for(double d: hwScores) {
+				avgHW += d;
 			}
 			
-			avg /= 25;
+			double avgLab = 0;
 			
-			//System.out.println("AVG = "+avg);
+			for(double d: labScores) {
+				avgLab += d;
+			}
 			
-			avg *= 10;
-			avg *= .75;
+			avgHW /= 20;
+			avgHW *= .65;
+			avgHW *= 10;
+			
+			avgLab /= 5;
+			avgLab *= .1;
+			avgLab *= 10;
 			
 			System.out.print("ENTER YOUR DESIRED FINAL GRADE IN COM_1300: ");
 			Scanner scan = new Scanner(System.in);
 			double desiredGrade = scan.nextDouble();
 			
-			double requiredGrade = (desiredGrade - avg) / .25;
+			double requiredGrade = (desiredGrade - avgHW - avgLab) / .25;
 			
-			double maxGrade = 25 + avg;
+			double maxGrade = 25 + avgHW + avgLab;
 			
 			System.out.printf("YOU WILL NEED A GRADE OF %.2f ON YOUR FINAL%n", requiredGrade);
 			System.out.printf("ASSUMING NO EXTRA-CREDIT ON THE FINAL, THE HIGHEST POSSIBLE GRADE IS %.2f%n", maxGrade);
