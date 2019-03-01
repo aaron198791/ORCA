570b7700b9acc030d48dbd3e688a9462e05a02b7
angel8818 <42220654+angel8818@users.noreply.github.com>
2019-02-28 22:50:21 -0500

    LeftRotation - itai and amram working on the hackerrank stuff from class

--- Projects/LeftRotation/sketch/school/LeftRotation.java
+++ Projects/LeftRotation/sketch/school/LeftRotation.java
@@ -6,9 +6,25 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class LeftRotation {
 
+    public static void rotate(int[] a, int d) {
+        int[] b = new int[a.length];
 
+        for(int i = 0; i < a.length; i++) {
+            if(i - d < 0) {
+                b[a.length + i - d] = a[i];
+            }
+
+            else {
+                b[i - d] = a[i];
+            }
+        }
+
+        for(int j = 0; j < a.length; j++) {
+            System.out.print(b[j] + " ");
+        } 
+    }
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -22,12 +38,14 @@ public class Solution {
         int[] a = new int[n];
 
         String[] aItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int i = 0; i < n; i++) {
             int aItem = Integer.parseInt(aItems[i]);
             a[i] = aItem;
         }
+
+        rotate(a, d);
 
         scanner.close();
     }
